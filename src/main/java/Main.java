984298c7e29db4b2828893f92f7d464947b9f715
tatufilip <tatufilip@yahoo.com>
2019-11-18 19:10:04 +0200

    xD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.UserRepository;
+
 public class Main {
+     static UserRepository userRepository;
     public static void main(String[] args) {
-        System.out.println("Whatever");
     }
 }
---
+++ src/main/java/exception/Messages.java
@@ -0,0 +1,5 @@
+package exception;
+
+public final class Messages{
+    public static final String USER_NOT_FOUND = "User with given id not found";
+}

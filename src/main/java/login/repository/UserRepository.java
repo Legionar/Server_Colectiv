628b13099bee6fb476f15190836cdc975058a9c7
Teo Radoi <andrei.teodor98@yahoo.com>
2019-12-09 21:13:21 +0200

    Fixed app not starting

--- src/main/java/login/repository/UserRepository.java
+++ src/main/java/login/repository/UserRepository.java
@@ -2,7 +2,17 @@ package login.repository;
 
 import login.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    User loginByEmail(String email, String password);
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u from User u WHERE u.email = :#{#email} and u.password = :#{#password}")
+    User loginByEmail(@Param("email") String email, @Param("password") String password);
 }
--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -2,10 +2,14 @@ package start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("login.rest")
 @SpringBootApplication
+@EnableJpaRepositories("login.repository")
+@EntityScan("login")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);

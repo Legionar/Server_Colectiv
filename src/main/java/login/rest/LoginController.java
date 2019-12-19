e71d86c702ef4692b5d650a79fcbc37fb8016f1e
ursurares <urares31@gmail.com>
2019-12-19 18:35:21 +0200

    removed supervisor and profile picture from login request

--- src/main/java/login/rest/LoginController.java
+++ src/main/java/login/rest/LoginController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @ComponentScan("login.service")
+@CrossOrigin
 public class LoginController {
 
     private final UserService userService;
--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @ComponentScan("login.rest")
 @SpringBootApplication
 @EnableJpaRepositories("login.repository")
 @EntityScan("login")
+@CrossOrigin
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);

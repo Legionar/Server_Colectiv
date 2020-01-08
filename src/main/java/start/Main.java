8381922ed8075f42bc7cefd3dc6fe1a33eebe4a6
Teo Radoi <andrei.teodor98@yahoo.com>
2020-01-08 16:33:50 +0200

    Added cross origin support

--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @ComponentScan({"login.rest","profile.rest"})
 @EnableJpaRepositories({"login.repository","profile.repository"})
 @EntityScan({"login.entity","profile.entity"})
+@CrossOrigin("*")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
